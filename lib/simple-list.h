956c411b15642916702c46f5425ca04657550cb0
Elmono56 <78763533+Elmono56@users.noreply.github.com>
2021-09-14 16:29:24 -0600

    update add

--- lib/simple-list.h
+++ lib/simple-list.h
@@ -20,4 +20,61 @@ struct simplelist {
         return size;
     }
 
-}
+    bool addPlayer(int pNumber, string pNombre) {
+        player playern = {pNumber, pNombre};
+        nodo nodon = {playern};
+        int result = insert(nodon, POSITION_END);
+        return result!=0;
+    }
+
+    void insertPlayer(int pNumber, string pNombre, int pPosition){
+        player playern = {pNumber, pNombre};
+        nodo nodon = {playern};
+        int result = insert(nodon, pPosition);
+        return result==size;
+    }
+
+    int insert(player pNodo, int pPosition) {
+        nodo* newvalue = (nodo*)malloc(sizeof(struct nodo));
+        newvalue->data = pNodo;
+        int result = 0;
+
+        if (size==0) {
+            start = newvalue;
+            end = newvalue;  
+        } else if (pPosition==0) {
+            newvalue->next = start;
+            start = newvalue;
+        } else {
+            int actualPosition = 1;
+            nodo* pointerToPosition = nullptr;
+            nodo* pointerBehind = nullptr;
+
+            if (pPosition>=size) {
+                pointerBehind = end;
+                end = newvalue;
+                actualPosition = size;
+            } else {
+                pointerToPosition = start->next;
+                pointerBehind = start;
+            }
+
+            while (actualPosition<pPosition && pointerToPosition!=nullptr) {
+                pointerBehind = pointerToPosition;
+                pointerToPosition = pointerToPosition->next;
+                actualPosition++;
+            }
+
+            pointerBehind->next = newvalue;
+            newvalue->next = pointerToPosition;
+            result = actualPosition;
+        }
+
+        size++;
+        return result;
+    }
+
+};
+
+
+#endif
