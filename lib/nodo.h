4404d703c50d0acf5013260e51b37e0a053b6868
Elmono56 <78763533+Elmono56@users.noreply.github.com>
2021-09-14 16:52:42 -0600

    update

--- lib/nodo.h
+++ lib/nodo.h
@@ -6,14 +6,14 @@
 struct player {
     int number;
     string name;
-}
+};
 
 struct nodo {
     player data;
     nodo* next = nullptr;
 
     string printPlayers(){
-        return (data.numer << "    " << data.name);
+        return (data.numer + "    "  + data.name);
     }
 };
 
