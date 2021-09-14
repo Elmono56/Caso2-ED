8ed11af1bb90741207a75d9fbcdb465e1dc8b55b
Elmono56 <78763533+Elmono56@users.noreply.github.com>
2021-09-14 16:46:26 -0600

    attempt1

---
+++ caso2main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "lib/simple-list.h"
+#include "lib/nodo.h"
+
+using namespace std;
+
+int main() {
+
+    simplelist milista;
+
+
+    milista.addPlayer(23,"Juan");
+    milista.addPlayer(10,"Luis");
+    milista.addPlayer(2,"Pedro");
+
+
+    milista.insertPlayer(20,"Pablo",0);
+    milista.insertPlayer(10,"Carlos",0);
+    milista.insertPlayer(5,"Jorge",0);
+
+    int posicioninsertada = milista.insertPlayer(65,"Andres",10);
+    cout << posicioninsertada << endl;
+
+    posicioninsertada = milista.insertPlayer(15,"Nicho",4);
+    cout << posicioninsertada << endl;
+
+    posicioninsertada = milista.insertPlayer(17,"Miguel",9);
+    cout << posicioninsertada << endl;
+
+
+
+    cout << milista.isEmpty() << " " << milista.getQuantity() << endl;
+
+}
